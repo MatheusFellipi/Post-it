d57ff834f3c4014f646e67ab6cf3b0c97c62ee6c
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-03 17:18:34 -0300

    Criando os servidor.

---
+++ src/Shared/Infra/Http/Server.ts
@@ -0,0 +1,19 @@
+import express, { Express } from "express";
+import "express-async-errors";
+
+export class Server {
+  app: Express;
+  port: string;
+  
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || "3333";
+  }
+
+  Start() {
+    this.app.listen(this.port, () => {
+      console.log("running server " + this.port);
+    });
+  }
+  
+}
