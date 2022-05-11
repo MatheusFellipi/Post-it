e7ce21af1bc3114353e253ead27b5ac4861bfdec
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-11 16:34:09 -0300

    Criando a tabela de dados de post it.

--- src/Shared/Infra/Typeorm/1651677757715-Migrations.ts
+++ src/Shared/Infra/Typeorm/1651677757715-Migrations.ts
@@ -4,16 +4,19 @@ export class Migrations1651677757715 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "post",
+        name: "postit",
         columns: [
-          {
-            name: "id",
-            type: "uuid",
-          },
+          { name: "id", type: "uuid" },
+          { name: "titulo", type: "varchar" },
+          { name: "description", type: "text" },
+          { name: "finalized", type: "boolean", default: "false" },
+          { name: "created_at", type: "timestamp", default: "now()" },
         ],
       })
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("post");
+  }
 }
