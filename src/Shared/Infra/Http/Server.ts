69f6f5d5f881a9f16f1012fd4322c7a91fff748b
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-11 19:14:09 -0300

    configurando menssagem de error do lado do servido

--- src/Shared/Infra/Http/Server.ts
+++ src/Shared/Infra/Http/Server.ts
@@ -1,4 +1,5 @@
-import express, { Express } from "express";
+import { AppError } from "@Shared/Errors/AppError";
+import express, { Express, NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { Routes } from "./Routes";
 // import "@Shared/Infra/Typeorm";
@@ -20,6 +21,18 @@ export class Server {
 
   Start() {
     this.routes();
+
+    this._app.use((error: Error, request: Request, response: Response) => {
+      if (error instanceof AppError) {
+        return response
+          .status(error.statusCode)
+          .json({ message: error.message });
+      }
+      return response.status(500).json({
+        status: "erro",
+        message: `Interna server - ${error.message}`,
+      });
+    });
 
     this._app.listen(this.port, () => {
       console.log("running server " + this.port);
