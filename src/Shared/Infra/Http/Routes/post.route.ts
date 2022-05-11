f1ff14cb98e2a1fdd1e1b10a0d20da874ab468ca
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-11 17:27:15 -0300

    Testa se as rotas esta funcionado

--- src/Shared/Infra/Http/Routes/post.route.ts
+++ src/Shared/Infra/Http/Routes/post.route.ts
@@ -1,5 +1,16 @@
+import { Router } from "express";
 
 export class PostRoutes {
-  constructor() {}
+  public router: Router;
 
+  constructor(router: Router) {
+    this.router = router;
+    this.route();
+  }
+
+  route() {
+    this.router.get("/", function (req, res) {
+      res.send("Hello World!");
+    });
+  }
 }
