cea686b93b915e17ac0be4455cd729ef47def176
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-11 16:37:00 -0300

    Craindo a entitate do post.

---
+++ src/Modules/Post/Infra/Typeorm/Entities/Postit.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("postit")
+class PostIt {
+  @PrimaryColumn("uuid")
+  id!: string;
+
+  @Column("varchar")
+  titulo!: string;
+
+  @Column("varchar")
+  description!: string;
+
+  @Column("varchar")
+  finalized!: string;
+
+  @UpdateDateColumn()
+  update_at!: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { PostIt };
