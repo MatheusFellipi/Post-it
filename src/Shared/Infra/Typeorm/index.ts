62172643bf61d65892a2751a3cc325d5e55f5e8b
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-13 19:36:37 -0300

    Configurando o bando de dados

--- src/Shared/Infra/Typeorm/index.ts
+++ src/Shared/Infra/Typeorm/index.ts
@@ -1,22 +1,10 @@
+import { dbConfig } from "@Config/db";
 import { DataSource } from "typeorm";
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  port: 5432,
-  host: "localhost",
-  username: "docker",
-  password: "post",
-  database: "post",
-  entities: ["./src/Modules/**/Entities/*.ts"],
-  migrations: ["./src/Shared/Infra/Typeorm/Migrations/*.ts"],
-  synchronize: true,
-  logging: false,
-  migrationsTableName: "./src/Shared/Infra/Typeorm/Migrations",
-});
+const AppDataSource = new DataSource(dbConfig);
 
 AppDataSource.initialize()
   .then((res) => {
-    // here you can start to work with your database
     console.log("running database");
   })
   .catch((error) => console.log(error));
