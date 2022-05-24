09a6620c4d767a821bd098059b16421ca14fa913
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-24 17:13:51 -0300

    Alterando o nomes do arquivo e os atributos

--- src/shared/infra/typeorm/migrations/1651677757715-Migrations.ts
+++ src/shared/infra/typeorm/migrations/1651677757715-Migrations.ts
@@ -9,7 +9,7 @@ export class Migrations1651677757715 implements MigrationInterface {
           { name: "id", type: "uuid" },
           { name: "titulo", type: "varchar" },
           { name: "description", type: "text" },
-          { name: "finalized", type: "boolean", default: "false" },
+          { name: "status", type: "varchar", default: "false" },
           { name: "created_at", type: "timestamp", default: "now()" },
         ],
       })
