d69c537cd12899f64f6804237e969f168286fb2d
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-24 19:49:30 -0300

    Correção de importação

--- src/Shared/Container/index.ts
+++ src/Shared/Container/index.ts
@@ -1,7 +1,9 @@
 import { container, delay } from "tsyringe";
 
-import { IPostRepository } from "modules/post/repositories/IPostRepository";
-import { PostItRepository } from "modules/post/infra/typeorm/repositories/PostRepository";
+import { IPostRepository } from "@modules/post/repositories/iPostRepository";
+import { PostItRepository } from "@modules/post/infra/typeorm/repositories/postRepository";
+
+
 
 container.registerSingleton<IPostRepository>(
   "PostRepository",
