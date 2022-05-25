5c232c225a239a7f778490c798ec2bba0850264f
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-24 21:33:06 -0300

    Alterando o atributos

--- src/shared/infra/typeorm/migrations/1651677757715-Migrations.ts
+++ src/shared/infra/typeorm/migrations/1651677757715-Migrations.ts
@@ -7,7 +7,7 @@ export class Migrations1651677757715 implements MigrationInterface {
         name: "postit",
         columns: [
           { name: "id", type: "uuid" },
-          { name: "titulo", type: "varchar" },
+          { name: "title", type: "varchar" },
           { name: "description", type: "text" },
           { name: "status", type: "varchar", default: "false" },
           { name: "created_at", type: "timestamp", default: "now()" },
@@ -17,6 +17,6 @@ export class Migrations1651677757715 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("post");
+    await queryRunner.dropTable("postit");
   }
 }
